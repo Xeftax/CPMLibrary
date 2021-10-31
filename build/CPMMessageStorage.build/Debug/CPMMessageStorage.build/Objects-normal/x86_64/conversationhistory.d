505a8e8452613e8be0311a3148e7eed44ab4ef8b
Jehan Monnier <jehan.monnier@linphone.org>
2021-10-31 17:11:59 +0100

    remove build dir

--- build/CPMMessageStorage.build/Debug/CPMMessageStorage.build/Objects-normal/x86_64/conversationhistory.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/conversationhistory.cpp \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/conversationhistory.h \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/cpmobject.h
--- build/CPMMessageStorage.build/Debug/CPMMessageStorage.build/Objects-normal/x86_64/groupstate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/groupstate.cpp \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/groupstate.h \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/cpmobject.h
