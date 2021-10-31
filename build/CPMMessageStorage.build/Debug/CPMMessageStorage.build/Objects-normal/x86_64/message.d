a7758dbd6f70f19a538b23b63a305c95f9b32c58
Jehan Monnier <jehan.monnier@linphone.org>
2021-10-31 17:07:14 +0100

    remove build dir

--- build/CPMMessageStorage.build/Debug/CPMMessageStorage.build/Objects-normal/x86_64/message.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/message.cpp \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/message.h \
-  /Users/quentin/Desktop/CPM\ message\ storage/CPMLibrary/src/Objects/include/cpmobject.h
